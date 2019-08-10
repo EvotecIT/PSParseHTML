1469c8e8733ba03800c65dee40e8e3ba3e5222c2
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-10 23:28:00 +0200

    Update

---
+++ Examples/Example-Formatting.ps1
@@ -0,0 +1,3 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+Format-HTML -File 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWriteHTML\Examples\Example25-Toasts\Example25.html' -OutputFile $PSScriptRoot\Output\Example.Toasts.html
