3834e00bc220ea21cbdfce1839a3cff11ab7f980
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-12-07 16:20:00 +0100

    Update

---
+++ Examples/Example-ExtractingTextFromTagsEM.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+$URL = "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/em"
+$html = (Invoke-WebRequest -Uri $UrL).Content
+
+ConvertFrom-HTMLTag -Content $HTML -Tag 'em'
