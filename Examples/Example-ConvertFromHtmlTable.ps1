d3060d1fb21b0b5f2cb65476e11b06b017413dc9
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-12-08 12:27:06 +0100

    Updated examples

--- Examples/Example-ConvertFromHtmlTable.ps1
+++ Examples/Example-ConvertFromHtmlTable.ps1
@@ -2,13 +2,17 @@
 Import-Module .\PSParseHTML.psd1 -Force
 
 $html = (Invoke-WebRequest -Uri 'https://en.wikipedia.org/wiki/PowerShell').Content
-$tmp = ConvertFrom-HtmlTable -Content $html
+$Tables = ConvertFrom-HtmlTable -Content $html
+foreach ($Table in $Tables) {
+    $Table | Format-Table -AutoSize *
+}
 
+# Alternatively
+$Tables = ConvertFrom-HtmlTable -Url 'https://en.wikipedia.org/wiki/PowerShell'
 # If converting multiple tables, the output will look funky
 # since it is creating an array of different objects.
-$tmp[0] | Format-Table -AutoSize
-$tmp[1] | Format-Table -AutoSize
-$tmp[2] | Format-Table -AutoSize
-$tmp[3] | Format-Table -AutoSize
-$tmp[4] | Format-Table -AutoSize
+$Tables[0] | Format-Table -AutoSize
+$Tables[1] | Format-Table -AutoSize
+$Tables[2] | Format-Table -AutoSize
+$Tables[3] | Format-Table -AutoSize
 # ... etc
--- Examples/Table-PremierLeague.ps1
+++ Examples/Table-PremierLeague.ps1
@@ -2,3 +2,5 @@
 $Test | Format-Table -AutoSize *
 
 
+$Test = ConvertFrom-HtmlTable -Url 'https://www.goal.com/en-us/premier-league/table/2kwbbcootiqqgmrzs6o5inle5' -Engine AngleSharp
+$Test | Format-Table -AutoSize *
