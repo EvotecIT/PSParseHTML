be1fa0fd841176f987a6ca7a78c75d1b8c9dd4ff
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-12-08 10:01:33 +0100

    Additional examples

---
+++ Examples/Table-PremierLeague.ps1
@@ -0,0 +1,4 @@
+﻿$Test = ConvertFrom-HtmlTable -Url 'https://www.goal.com/en-us/premier-league/table/2kwbbcootiqqgmrzs6o5inle5'
+$Test | Format-Table -AutoSize *
+
+
---
+++ Examples/Table-UefaChampionsLeague.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+$Test = ConvertFrom-HtmlTable -Url 'https://www.goal.com/en-us/uefa-champions-league/table/4oogyu6o156iphvdvphwpck10'
+for ($Count = 0; $Count -lt $Test.Count; $Count++) {
+    $Test[$Count] | Format-Table -AutoSize *
+}
