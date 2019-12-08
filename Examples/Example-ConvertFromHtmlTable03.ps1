a50fcf06e8a78faa9710302bcaed4ff07f714b4f
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-12-08 16:04:09 +0100

    Examples update

---
+++ Examples/Example-ConvertFromHtmlTable03.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+# Method to extract special case tables
+$Tables1 = ConvertFrom-HtmlTable -Url 'https://en.wikipedia.org/wiki/PowerShell' -ReverseTable
+$Tables2 = ConvertFrom-HtmlTable -Url 'https://en.wikipedia.org/wiki/PowerShell'
+
+New-HTML {
+    New-HTMLTab -Name 'Reverse Table' {
+        foreach ($Table in $Tables1) {
+            New-HTMLTable -DataTable $Table -Filtering
+        }
+    }
+    New-HTMLTab -Name 'Non-Reverse Table' {
+        foreach ($Table in $Tables2) {
+            New-HTMLTable -DataTable $Table -Filtering
+        }
+    }
+} -FilePath "$PSScriptRoot\Output\Example.Wikipedia.html" -ShowHTML -UseCssLinks -UseJavaScriptLinks
