a371ce0241c5c741bf88bdc0d568ab9b777bcc49
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-02-12 19:52:08 +0100

    Update

---
+++ Examples/Example-ConvertFromHtmlAttributes.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+$URL = "https://www.dailydairyreport.com"
+$html = (Invoke-WebRequest -Uri $UrL).Content
+
+ConvertFrom-HTMLAttributes -Content $HTML -Class 'banner-text-info'
+
+$URL = "https://www.kintai-driver.com/"
+$html = (Invoke-WebRequest -Uri $UrL).Content
+
+ConvertFrom-HTMLAttributes -Content $HTML -Class 'phone-contact'
---
+++ Public/ConvertFrom-HTMLAttributes.ps1
@@ -0,0 +1,26 @@
+﻿function ConvertFrom-HTMLAttributes {
+    [alias('ConvertFrom-HTMLTag', 'ConvertFrom-HTMLClass')]
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory = $true)][string]$Content,
+        [string]$Tag,
+        [string]$Class
+    )
+    Begin {
+        # Initialize the parser
+        $HTMLParser = [AngleSharp.Html.Parser.HtmlParser]::new()
+    }
+    Process {
+        # Load the html
+        $ParsedDocument = $HTMLParser.ParseDocument($content)
+        # Get all the tables
+        if ($Tag) {
+            [Array] $TagContent = $ParsedDocument.GetElementsByTagName($Tag)
+            $TagContent.TextContent
+        } elseif ($Class) {
+            [Array] $ClassContent = $ParsedDocument.GetElementsByClassName($Class)
+            $ClassContent.TextContent
+        }
+    }
+    End { }
+}
