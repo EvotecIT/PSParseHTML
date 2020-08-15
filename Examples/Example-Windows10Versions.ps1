60ed03918c7e558e8a1c5841edbf74c70dbaf6a7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-15 22:09:46 +0200

    Examples update

--- Examples/Example-Windows10Versions.ps1
+++ Examples/Example-Windows10Versions.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module PSParseHTML
+﻿Import-Module .\PSParseHTML.psd1 -Force
 
 $Tables = ConvertFrom-HtmlTable -Url 'https://winreleaseinfoprod.blob.core.windows.net/winreleaseinfoprod/en-US.html'
 $Tables[0] | Format-Table
--- Examples/Table-PremierLeague.ps1
+++ Examples/Table-PremierLeague.ps1
@@ -1,4 +1,6 @@
-﻿$Test = ConvertFrom-HtmlTable -Url 'https://www.goal.com/en-us/premier-league/table/2kwbbcootiqqgmrzs6o5inle5'
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+$Test = ConvertFrom-HtmlTable -Url 'https://www.goal.com/en-us/premier-league/table/2kwbbcootiqqgmrzs6o5inle5'
 $Test | Format-Table -AutoSize *
 
 
