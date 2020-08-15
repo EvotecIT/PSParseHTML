976bff0d53e1bc92a7287cf2a9ecf9944208203d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-15 22:09:32 +0200

    Published manage-module

---
+++ Publish/Manage-Module.ps1
@@ -0,0 +1,157 @@
+﻿Clear-Host
+Import-Module "C:\Support\GitHub\PSPublishModule\PSPublishModule.psm1" -Force
+
+$Configuration = @{
+    Information = @{
+        ModuleName        = 'PSParseHTML'
+
+        DirectoryProjects = 'C:\Support\GitHub'
+        FunctionsToExport = 'Public'
+        AliasesToExport   = 'Public'
+
+        LibrariesCore     = 'Lib\Standard'
+        LibrariesDefault  = 'Lib\Standard'
+
+        Manifest          = @{
+            # Minimum version of the Windows PowerShell engine required by this module
+            PowerShellVersion    = '5.1'
+            # Supported PSEditions
+            CompatiblePSEditions = @('Desktop', 'Core')
+            # ID used to uniquely identify this module
+            GUID                 = 'f0387960-7034-4918-a1e1-d5847cbf90df'
+            # Version number of this module.
+            ModuleVersion        = '0.0.X'
+            # Author of this module
+            Author               = 'Przemyslaw Klys'
+            # Company or vendor of this module
+            CompanyName          = 'Evotec'
+            # Copyright statement for this module
+            Copyright            = '(c) Przemyslaw Klys. All rights reserved.'
+            # Description of the functionality provided by this module
+            Description          = 'Module that allows to manipulate, parse, format and optimize HTML, JavaScript and CSS'
+            # Tags applied to this module. These help with module discovery in online galleries.
+            Tags                 = @('HTML', 'WWW', 'JavaScript', 'CSS', 'Windows', 'MacOS', 'Linux')
+            # A URL to the main website for this project.
+            ProjectUri           = 'https://github.com/EvotecIT/PSParseHTML'
+
+            IconUri              = 'https://evotec.xyz/wp-content/uploads/2018/12/PSWriteHTML.png'
+        }
+    }
+    Options     = @{
+        Merge             = @{
+            Sort           = 'None'
+            FormatCodePSM1 = @{
+                Enabled           = $true
+                RemoveComments    = $false
+                FormatterSettings = @{
+                    IncludeRules = @(
+                        'PSPlaceOpenBrace',
+                        'PSPlaceCloseBrace',
+                        'PSUseConsistentWhitespace',
+                        'PSUseConsistentIndentation',
+                        'PSAlignAssignmentStatement',
+                        'PSUseCorrectCasing'
+                    )
+
+                    Rules        = @{
+                        PSPlaceOpenBrace           = @{
+                            Enable             = $true
+                            OnSameLine         = $true
+                            NewLineAfter       = $true
+                            IgnoreOneLineBlock = $true
+                        }
+
+                        PSPlaceCloseBrace          = @{
+                            Enable             = $true
+                            NewLineAfter       = $false
+                            IgnoreOneLineBlock = $true
+                            NoEmptyLineBefore  = $false
+                        }
+
+                        PSUseConsistentIndentation = @{
+                            Enable              = $true
+                            Kind                = 'space'
+                            PipelineIndentation = 'IncreaseIndentationAfterEveryPipeline'
+                            IndentationSize     = 4
+                        }
+
+                        PSUseConsistentWhitespace  = @{
+                            Enable          = $true
+                            CheckInnerBrace = $true
+                            CheckOpenBrace  = $true
+                            CheckOpenParen  = $true
+                            CheckOperator   = $true
+                            CheckPipe       = $true
+                            CheckSeparator  = $true
+                        }
+
+                        PSAlignAssignmentStatement = @{
+                            Enable         = $true
+                            CheckHashtable = $true
+                        }
+
+                        PSUseCorrectCasing         = @{
+                            Enable = $true
+                        }
+                    }
+                }
+            }
+            FormatCodePSD1 = @{
+                Enabled        = $true
+                RemoveComments = $false
+            }
+            Integrate      = @{
+                ApprovedModules = @('PSSharedGoods', 'PSWriteColor', 'Connectimo', 'PSUnifi', 'PSWebToolbox', 'PSMyPassword')
+            }
+        }
+        Standard          = @{
+            FormatCodePSM1 = @{
+
+            }
+            FormatCodePSD1 = @{
+                Enabled = $true
+                #RemoveComments = $true
+            }
+        }
+        ImportModules     = @{
+            Self            = $true
+            RequiredModules = $false
+            Verbose         = $false
+        }
+        PowerShellGallery = @{
+            ApiKey   = 'C:\Support\Important\PowerShellGalleryAPI.txt'
+            FromFile = $true
+        }
+        GitHub            = @{
+            ApiKey   = 'C:\Support\Important\GithubAPI.txt'
+            FromFile = $true
+            UserName = 'EvotecIT'
+            #RepositoryName = 'PSWriteHTML'
+        }
+        Documentation     = @{
+            Path       = 'Docs'
+            PathReadme = 'Docs\Readme.md'
+        }
+    }
+    Steps       = @{
+        BuildModule        = @{
+            Enable              = $true
+            DeleteBefore        = $true
+            Merge               = $true
+            MergeMissing        = $true
+            SignMerged          = $true
+            Releases            = $true
+            LibrarySeparateFile = $false
+            ReleasesUnpacked    = $false
+            RefreshPSD1Only     = $false
+        }
+        BuildDocumentation = $false
+        PublishModule      = @{
+            Enabled      = $false
+            Prerelease   = ''
+            RequireForce = $false
+            GitHub       = $false
+        }
+    }
+}
+New-PrepareModule -Configuration $Configuration -Verbose
