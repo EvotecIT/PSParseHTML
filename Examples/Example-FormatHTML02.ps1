26417d00907ee479ed95762e83af86d0fe3a9090
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-11-15 22:02:56 +0100

    Update FormatHTML examples

---
+++ Examples/Example-FormatHTML02.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+Optimize-HTML -File $PSScriptRoot\Output\Example.Formatted.html -OutputFile $PSScriptRoot\Output\Example.Minified.html
+Format-HTML -File $PSScriptRoot\Output\Example.Formatted.html -OutputFile $PSScriptRoot\Output\Example.Formatted1.html #-RemoveHTMLComments
+
+#Format-HTML -File $PSScriptRoot\Output\Example.Formatted2.html -OutputFile $PSScriptRoot\Output\Example.Formatted3.html
