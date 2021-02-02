c95cbfea976c068b319111d819e11036ee3e5dc8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-02 20:09:40 +0100

    Update example output

--- Examples/Example-FormatHTML05.ps1
+++ Examples/Example-FormatHTML05.ps1
@@ -1,4 +1,8 @@
 ﻿Import-Module .\PSParseHTML.psd1 -Force
 
-Format-HTML -File $PSScriptRoot\Input\Test.html #-OutputFile $PSScriptRoot\Input\Test1.html
-Optimize-HTML -File $PSScriptRoot\Input\Test.html
+New-HTML -Online {
+    New-HTMLTable -DataTable 'test'
+} -FilePath $PSScriptRoot\Input\Test.html #-ShowHTML
+
+Format-HTML -File $PSScriptRoot\Input\Test.html -OutputFile $PSScriptRoot\Input\Test1.html
+Optimize-HTML -File $PSScriptRoot\Input\Test.html -OutputFile $PSScriptRoot\Input\Test2.html
