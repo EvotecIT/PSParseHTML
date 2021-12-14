02235f988e9e62408efebe109805136c8838e0b4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-14 19:06:37 +0100

    Added example for new function

---
+++ Examples/Example-ConvertFromHTML.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+# Option 1 - uses Agility Pack
+$PageHTML = ConvertFrom-HTML -URL "https://www.evotec.xyz"
+$PageHTML
+
+# Option 2 - uses AngleSharp
+$PageHTML = ConvertFrom-HTML -URL "https://www.evotec.xyz" -Engine AngleSharp
+$PageHTML
