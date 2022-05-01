0f3d1436fab3441d7633b0017071c0c2e9922d4c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-05-01 16:01:26 +0200

    Update examples

---
+++ Examples/Example-ExtractMeta.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+$File = 'C:\Support\GitHub\ADDashboard\Ignore\Reports\Report.html'
+$Objects = ConvertFrom-HTMLAttributes -Content (Get-Content -Raw $File) -Tag 'meta' -ReturnObject
+foreach ($O in $Objects) {
+    [PSCUstomObject] @{
+        name    = $O.name
+        content = $O.content
+        comment = $O.comment
+    }
+}
+
+
+$Objects = ConvertFrom-HTMLAttributes -Content (Get-Content -Raw $File) -Tag 'style' -ReturnObject
+foreach ($O in $Objects) {
+    [PSCUstomObject] @{
+        name    = $O.name
+        content = $O.content
+        comment = $O.comment
+    }
+}
---
+++ Examples/Example-GetLinks.ps1
@@ -0,0 +1,13 @@
+﻿$URL = "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/em"
+$html = (Invoke-WebRequest -Uri $UrL).Content
+
+$Classes = ConvertFrom-HTMLAttributes -Content $HTML -Class 'sidebar' -ReturnObject
+
+$Objects = ConvertFrom-HTMLTag -Content $Classes[0].InnerHtml -Tag 'a' -ReturnObject
+$Objects.href
+
+
+
+$URL = "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/em"
+$html = (Invoke-WebRequest -Uri $UrL).Content
+$Objects = ConvertFrom-HTMLTag -Content $html -Tag 'option' #-ReturnObject
