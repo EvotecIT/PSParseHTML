a552a5d4dce1ba6df40040dd5316b94a5b7efe19
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-25 10:34:01 +0100

    Add another example

---
+++ Examples/Example-ConvertFromHtmlTable07.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+$Url = 'https://learn.microsoft.com/en-us/officeupdates/update-history-microsoft365-apps-by-date#version-history'
+
+$AllTables = ConvertFrom-HtmlTable -Url $Url -Engine AgilityPack
+$AllTables.Count
+$AllTables[0] | Format-Table
+$AllTables[1] | Format-Table
