ef0c8f879be2693ae1cbb66e0e7863c42a2cf088
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-12 14:43:38 +0100

    Optimized CSS code in tests

--- Tests/Optimize-Css.Tests.ps1
+++ Tests/Optimize-Css.Tests.ps1
@@ -63,9 +63,7 @@
         }
 "@
 
-        $ExpectedOutput = @"
-.tabsWrapper{text-align:center;margin:10px auto;font-family:'Roboto',sans-serif !important}.tabs{margin-top:10px;font-size:15px;padding:0;list-style:none;background:#fff;box-shadow:0 5px 20px rgba(0,0,0,.1);border-radius:4px;position:relative}.tabs .round{border-radius:4px}.tabs a{text-decoration:none;color:#777;text-transform:uppercase;padding:10px 20px;display:inline-block;position:relative;z-index:1;transition-duration:.6s}.tabs a.active{color:#fff}.tabs a i{margin-right:5px}.tabs .selector{display:none;height:100%;position:absolute;left:0;top:0;right:0;bottom:0;z-index:1;border-radius:4px;background:ColorSelector}.tabs-content{display:none}.tabs-content.active{display:block}
-"@
+        $ExpectedOutput = ".tabsWrapper{text-align:center;margin:10px auto;font-family:'Roboto',sans-serif!important}.tabs{margin-top:10px;font-size:15px;padding:0;list-style:none;background:#fff;box-shadow:0 5px 20px rgba(0,0,0,.1);border-radius:4px;position:relative}.tabs .round{border-radius:4px}.tabs a{text-decoration:none;color:#777;text-transform:uppercase;padding:10px 20px;display:inline-block;position:relative;z-index:1;transition-duration:.6s}.tabs a.active{color:#fff}.tabs a i{margin-right:5px}.tabs .selector{display:none;height:100%;position:absolute;left:0;top:0;right:0;bottom:0;z-index:1;border-radius:4px;background:ColorSelector}.tabs-content{display:none}.tabs-content.active{display:block}"
         $Output = Optimize-CSS -Content $CssCode
         $Output | Should -Be $ExpectedOutput
     }
