11e03c511fba3cf5ed6ba6b233b694c730d6441b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-19 16:55:04 +0100

    cleanup from tries

--- Sources/PSParseHTML/AngleSharp.cs
+++ Sources/PSParseHTML/AngleSharp.cs
@@ -11,36 +11,5 @@ using AngleSharp.Dom;
 namespace PSParseHTML {
     public class AngleSharp {
 
-        //public static async Task Test() {
-
-        //    string address = "https://www2.asx.com.au/markets/trade-our-cash-market/directory";
-
-
-        //    IConfiguration config = Configuration.Default
-        //        .WithDefaultLoader()
-        //        .WithJs();
-        //    IBrowsingContext context = BrowsingContext.New(config);
-
-        //    // open document
-        //    var document = await context.OpenAsync(address).WaitUntilAvailable();
-
-        //    var numEntries = document.ExecuteScript("document.querySelectorAll('div').length");
-
-        //    string s;
-        //    s = "th";
-
-        //    // get list of elements in document that match the selector (or selector group)
-        //    IHtmlCollection<IElement> elements = document.QuerySelectorAll(s);
-
-        //    // get the text
-        //    IEnumerable<string> elementsText = elements.Select(m => m.TextContent);
-
-        //    // debug dump
-        //    Console.WriteLine("MatchedElements = " + elements.Length.ToString());
-        //    foreach (string text in elementsText)
-        //        Console.WriteLine(text);
-
-        //    Console.WriteLine("DONE");
-        //}
     }
 }
--- Sources/PSParseHTML/HtmlAgilityPack.cs
+++ Sources/PSParseHTML/HtmlAgilityPack.cs
@@ -7,40 +7,5 @@ using static System.Net.Mime.MediaTypeNames;
 namespace PSParseHTML {
     public class HtmlAgilityPack {
 
-        //private void LoadHtmlWithBrowser(String url) {
-        //    webBrowser1.ScriptErrorsSuppressed = true;
-        //    webBrowser1.Navigate(url);
-
-        //    waitTillLoad(this.webBrowser1);
-
-        //    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-        //    var documentAsIHtmlDocument3 = (mshtml.IHTMLDocument3)webBrowser1.Document.DomDocument;
-        //    StringReader sr = new StringReader(documentAsIHtmlDocument3.documentElement.outerHTML);
-        //    doc.Load(sr);
-        //}
-
-        //private void waitTillLoad(WebBrowser webBrControl) {
-        //    WebBrowserReadyState loadStatus;
-        //    int waittime = 100000;
-        //    int counter = 0;
-        //    while (true) {
-        //        loadStatus = webBrControl.ReadyState;
-        //        Application.DoEvents();
-        //        if ((counter > waittime) || (loadStatus == WebBrowserReadyState.Uninitialized) || (loadStatus == WebBrowserReadyState.Loading) || (loadStatus == WebBrowserReadyState.Interactive)) {
-        //            break;
-        //        }
-        //        counter++;
-        //    }
-
-        //    counter = 0;
-        //    while (true) {
-        //        loadStatus = webBrControl.ReadyState;
-        //        Application.DoEvents();
-        //        if (loadStatus == WebBrowserReadyState.Complete && webBrControl.IsBusy != true) {
-        //            break;
-        //        }
-        //        counter++;
-        //    }
-        //}
     }
 }
